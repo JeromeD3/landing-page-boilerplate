12e0f495b6250b986ab0e5b83f8cd428296db5a4
Jerome <1760399176@qq.com>
2024-04-30 11:26:29 +0800

    Revert "chore: remove .."
    This reverts commit 75ccbca40079a58c3cfb952edd5828821c1f8e77.

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)?', // Matches all pages
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'allow-from',
+          }
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
