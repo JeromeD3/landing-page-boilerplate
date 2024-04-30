75ccbca40079a58c3cfb952edd5828821c1f8e77
Jerome <1760399176@qq.com>
2024-04-30 11:21:57 +0800

    chore: remove ..

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)?', // Matches all pages
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'allow-from',
-          }
-        ]
-      }
-    ]
-  }
 };
 
 export default nextConfig;
